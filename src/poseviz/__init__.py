a5f8b4fc1ae02269c68bd35d395edae44af75573
Istvan Sarandi <sarandi@vision.rwth-aachen.de>
2026-03-19 22:59:16 +0100

    Configure setuptools-scm version file generation
    Add tool.setuptools_scm config to pyproject.toml to generate
    _version.py, expose __version__ in __init__.py, and gitignore
    the generated file.

--- src/poseviz/__init__.py
+++ src/poseviz/__init__.py
@@ -3,4 +3,9 @@
 from poseviz.poseviz import PoseViz
 from poseviz.view_info import ViewInfo
 
-__all__ = ["PoseViz", "ViewInfo"]
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = '0.0.0'
+
+__all__ = ["PoseViz", "ViewInfo", "__version__"]
